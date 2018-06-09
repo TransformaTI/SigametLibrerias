70d3615204f54cca519ac3450dcf513707a60b0e
Iván Trujillo <itrujillo@softgrade.mx>
2018-06-09 16:24:26 -0500

    Se implementa la eliminación de cobros por parte del usuario

--- LiquidacionPortatil/frmLiquidacionPortatil.vb
+++ LiquidacionPortatil/frmLiquidacionPortatil.vb
@@ -6023,7 +6023,13 @@ Public Class frmLiquidacionPortatil
     End Sub
 
     Private Sub btnCancelarPago_Click(sender As Object, e As EventArgs) Handles btnCancelarPago.Click
-        Close()
+
+        If MessageBox.Show("¿Realmente desea eliminar los cobros capturados para la liquidación de esta ruta?", Me.Text, MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk) = DialogResult.OK Then
+            _listaCobros.Clear()
+            ActualizarTotalizadorFormasDePago(_listaCobros)
+            Close()
+        End If
+
     End Sub
 
 
