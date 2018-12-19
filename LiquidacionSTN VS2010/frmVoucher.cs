023c0844cffff709518b4c5ddd4ce5c61c218f1b
Ruben Morera <rubencho_01@hotmail.com>
2018-12-19 10:43:52 -0600

    No calcular el saldo al dar click en el botón Aceptar, pantalla Voucher

--- LiquidacionSTN VS2010/frmVoucher.cs
+++ LiquidacionSTN VS2010/frmVoucher.cs
@@ -711,7 +711,7 @@ namespace LiquidacionSTN
         {
             try
             {
-                CalcularSaldo();
+                //CalcularSaldo();
                 Aceptar();
             }
             catch (Exception ex)
@@ -741,10 +741,17 @@ namespace LiquidacionSTN
                 if (saldo < 0)
                     saldo = 0;
 
-                if (EsCorrectoElMonto())
-                    _Saldo = saldo;
+                if (_Monto != _Total)
+                {
+                    if (EsCorrectoElMonto())
+                        _Saldo = saldo;
+                    else
+                        _Saldo = 0;
+                }
                 else
-                    _Saldo = 0;
+                {
+                    _Saldo = saldo;
+                }
 
                 txtSaldo.Text = _Saldo.ToString();
             }
