81d5c4417c673072a3456759383a7f4b3e8c5af7
Enrique Vargas <evargas@binarycode.company>
2018-12-19 21:22:37 -0600

    Parallel Kike

--- ConfirmacionProgramados/GasMetropolitano.Runtime/UISigametWEB/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace UISigametWEB.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- STClienteEquipo/Module.vb
+++
@@ -1,5 +0,0 @@
-Imports System.Data.SqlClient
-Module _Module
-    'Public ConString As String = SigaMetClasses.LeeInfoConexion(False)
-    Public cnSigamet As SqlConnection = SigaMetClasses.DataLayer.Conexion
-End Module
